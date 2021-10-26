65b31d2d36b1a0af38ee5dd168cbfffa0cee92f6
Prajneya <prajneya.kumar@research.iiit.ac.in>
2021-10-26 19:11:19 +0530

    fcfs and strace

---
+++ user/setpriority.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char* argv[])
+{	
+
+	if(argc != 3){
+	    fprintf(2, "Usage: %s priority command\n", argv[0]);
+	    exit(1);
+	}
+	else{
+		int prev_priority = set_priority(atoi(argv[1]), atoi(argv[2]));
+    	printf("The priority of %d has changed from %d to %d \n",atoi(argv[1]), prev_priority, atoi(argv[2]));
+    	exit(0);
+	}
+    
+}
